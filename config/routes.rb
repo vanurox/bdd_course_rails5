f8954f488229fae5cd94dd4e717d78c8f453e00f
Varun Sharma <cto.varun@gmail.com>
2016-12-26 13:11:02 +0000

    Create Article Successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/index'
+
   get 'users/new'
 
   get 'users/show'
 
   get 'users/view'
+
+  root to: "articles#index"
+
+  resources :articles
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
